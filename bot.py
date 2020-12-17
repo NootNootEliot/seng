59f0b987799848a9d39fa6c1b9f6e9fdd0757199
paul <paul@renaudassociates.co.za>
2020-12-17 20:52:02 +0200

    Revert bot commit
    Revert "bot.py"
    
    This reverts commit 9fe1dda5370c0be0ba3953498b2cec550397b6e4.

--- bot.py
+++ bot.py
@@ -10,43 +10,10 @@ from cogs.help import GetHelp
 
 bot = commands.Bot(command_prefix='$')
 
-# Add in intent to allow us to monitor member stats
-
-intents = discord.Intents.default()
-intents.members = True
 
 @bot.event
 async def on_ready():
     print("Ready to go!")
-
-
-    #
-    #
-    # List ALL 'online' member
-    #
-    #
-
-
-    guild_name = "ExplorerTest"
-    # guild_name = "Astra Nova Network"
-
-    for guild in bot.guilds:
-        if guild.name == guild_name:
-            break
-    
-    true_member_count = 0
-    member_online_count = 0
-
-    async for member in guild.fetch_members(limit=None):
-        # print(member)
-        if not member.bot:
-            # print(member) - lists all non bot members
-            true_member_count += 1
-            if member.status != "offline":
-                member_online_count += 1
-
-    print(f'Total member count: {true_member_count}')
-    print(f'Online member count: {member_online_count}')
 
 
 # Read in the token.
