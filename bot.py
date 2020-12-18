7a8030e9410314382669cc7ffa4c34b79ed06a20
paul <paul@renaudassociates.co.za>
2020-12-18 20:50:11 +0200

    Remove redundant code and comments and implement as command

--- bot.py
+++ bot.py
@@ -7,54 +7,60 @@ from cogs.send_welcome import Welcome
 from cogs.validation import ModeratorChecking
 from cogs.error_handler import CommandErrorHandler
 from cogs.help import GetHelp
+# from .validation import is_moderator, is_mod_commands_channel
 
 # Add in intent to allow us to monitor member stats
 
 intents = discord.Intents.default()
 intents.members = True
 
-
 bot = commands.Bot(command_prefix='$')
-
 
 @bot.event
 async def on_ready():
     print("Ready to go!")
 
-    # ---------------------------------------------------
-    # List true member count and 'online' members
-    # ---------------------------------------------------
+class MemberStats(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
 
-    #guild_name = "ExplorerTest"
-    guild_name = "Astra Nova Network"
+    @commands.command()
 
-    for guild in bot.guilds:
-        if guild.name == guild_name:
-            break
+    async def mstats(self, ctx):
 
-    true_member_count = 0
-    member_online_count = 0
+        #if not await is_moderator(ctx):
+        #    return
+        #if not await is_mod_commands_channel(ctx):
+        #    return
 
-    async for member in guild.fetch_members(limit=None):
-        # print(member)
-        if not member.bot:
-            # print(member) - lists all non bot members
-            true_member_count += 1
-            if member.status != "offline":
-                member_online_count += 1
-    print(f'Total member count: {true_member_count}')
-    print(f'Online member count: {member_online_count}')
+        with open('./server_specific/channel_ids.json', 'r') as id_file:
+            channel_id_dict = json.loads(id_file.read())
+            
+            guild_id = channel_id_dict['GUILD']
+            welcome_id = channel_id_dict['WELCOME']
+            mbc_id = channel_id_dict['MOD_COMMANDS']
 
-    # ---------------------------------------------------
-    #
-    # ---------------------------------------------------
+            guild = self.bot.get_guild(guild_id)
+            welcome_channel = guild.get_channel(welcome_id)
+     
+            true_member_count = 0
+            member_online_count = 0
+
+            async for member in guild.fetch_members(limit=None):
+                if not member.bot:
+                    true_member_count += 1
+                    if member.status != "offline":
+                        member_online_count += 1
+
+            await ctx.send(f'Total members: {true_member_count}')
+            await ctx.send(f'Online members: {member_online_count}')
 
 # Read in the token.
 with open(Path('./private/priv_data.json'), 'r') as data_file:
     token = json.loads(data_file.read())['TOKEN']
 
 bot.remove_command('help')  # Overwrite normal 'help' command
-#bot.add_cog(CommandErrorHandler(bot))
+bot.add_cog(MemberStats(bot))
 bot.add_cog(Greetings(bot))
 bot.add_cog(Welcome(bot))
 bot.add_cog(ModeratorChecking(bot))
