895b012e28ab7dd2e720d5bca12135a57dfcba8e
Eliot <63655810+NootNootEliot@users.noreply.github.com>
2020-12-21 08:37:40 +0000

    Revert "List guild members" (#2)

--- bot.py
+++ bot.py
@@ -7,16 +7,14 @@ from cogs.send_welcome import Welcome
 from cogs.validation import ModeratorChecking
 from cogs.error_handler import CommandErrorHandler
 from cogs.help import GetHelp
-from cogs.member_stats import MemberStats
-
-intents = discord.Intents.default()
-intents.members = True
 
 bot = commands.Bot(command_prefix='$')
+
 
 @bot.event
 async def on_ready():
     print("Ready to go!")
+
 
 # Read in the token.
 with open(Path('./private/priv_data.json'), 'r') as data_file:
@@ -24,7 +22,6 @@ with open(Path('./private/priv_data.json'), 'r') as data_file:
 
 bot.remove_command('help')  # Overwrite normal 'help' command
 #bot.add_cog(CommandErrorHandler(bot))
-bot.add_cog(MemberStats(bot))
 bot.add_cog(Greetings(bot))
 bot.add_cog(Welcome(bot))
 bot.add_cog(ModeratorChecking(bot))
