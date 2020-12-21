27a93eb1fb12bd18289370b382f4fd6fbac0bade
wouldlovetogetittowork <paul@renaudassociates.co.za>
2020-12-21 17:10:30 +0200

    List guild members (#3)
    Statistics for Members that are online.

--- bot.py
+++ bot.py
@@ -7,14 +7,16 @@ from cogs.send_welcome import Welcome
 from cogs.validation import ModeratorChecking
 from cogs.error_handler import CommandErrorHandler
 from cogs.help import GetHelp
+from cogs.member_stats import MemberStats
+
+intents = discord.Intents.default()
+intents.members = True
 
 bot = commands.Bot(command_prefix='$')
-
 
 @bot.event
 async def on_ready():
     print("Ready to go!")
-
 
 # Read in the token.
 with open(Path('./private/priv_data.json'), 'r') as data_file:
@@ -22,6 +24,7 @@ with open(Path('./private/priv_data.json'), 'r') as data_file:
 
 bot.remove_command('help')  # Overwrite normal 'help' command
 #bot.add_cog(CommandErrorHandler(bot))
+bot.add_cog(MemberStats(bot))
 bot.add_cog(Greetings(bot))
 bot.add_cog(Welcome(bot))
 bot.add_cog(ModeratorChecking(bot))
