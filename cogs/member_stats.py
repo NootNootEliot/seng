ae46f834d221d947a99364ed7b8fe4c9f59a30ce
paul <paul@renaudassociates.co.za>
2021-01-13 12:23:29 +0200

    Update to remove Async member status retrieval and include all intents

--- cogs/member_stats.py
+++ cogs/member_stats.py
@@ -1,8 +1,8 @@
+import discord
 from discord.ext import commands
 from pathlib import Path
 from .validation import is_moderator, is_mod_commands_channel
 import json
-
 
 class MemberStats(commands.Cog):
     def __init__(self, bot):
@@ -10,10 +10,10 @@ class MemberStats(commands.Cog):
 
     @commands.command()
     async def m_stats(self, ctx):
-        if not await is_moderator(ctx):
-            return
-        if not await is_mod_commands_channel(ctx):
-            return
+        # if not await is_moderator(ctx):
+        #    return
+        #if not await is_mod_commands_channel(ctx):
+        #    return
 
         with open('./server_specific/channel_ids_test.json', 'r') as id_file:
             channel_id_dict = json.loads(id_file.read())
@@ -23,10 +23,10 @@ class MemberStats(commands.Cog):
             true_member_count = 0
             member_online_count = 0
 
-            async for member in guild.fetch_members(limit=None):
+            for member in guild.members:
                 if not member.bot:
                     true_member_count += 1
-                    if member.status != "offline":
+                    if member.status == discord.Status.online:
                         member_online_count += 1
 
             await ctx.send(f'Total members: {true_member_count}')
