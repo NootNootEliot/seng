8f71d42115617d069f12232ba213d29355dfddf1
Eliot <eliotpersonal77@gmail.com>
2021-02-01 19:45:58 +0000

    Insert missing newline character into help string

--- cogs/help.py
+++ cogs/help.py
@@ -37,7 +37,7 @@ class GetHelp(commands.Cog):
             '`m_toggle_member_muted` - Toggles the \'Muted\' role on a '
             'Member\n'
             '`m_view_muted_members` - View all Members with the \'Muted\' '
-            'role.'
+            'role.\n'
             '`m_mute_perm_setup` - Sets up the mute permission setting on '
             'every channel in the guild.'
         )
