0309ae6501540cb5fde67d605b3e4f00cbfc4503
Eliot <eliotpersonal77@gmail.com>
2021-02-01 18:57:19 +0000

    Put things into methods to reduce repeated code
    This is quite messy I feel

--- cogs/mute_member.py
+++ cogs/mute_member.py
@@ -12,17 +12,70 @@ class MuteMember(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command()
-    async def m_mute_member(self, ctx):
-        """Mutes a Member"""
-        # Validation
-        if not await is_moderator(ctx):
-            return
-        if not await is_mod_commands_channel(ctx):
-            return
-        if not await is_process_and_user_clear(self.bot, 'm_mute_member', ctx):
-            return
+    async def get_uname_digits(self, ctx, message):
+        """Returns tuple of username and four digits from string
+
+        Expects a string with two backticks. Takes the part within the
+        backticks, and then splits it about the '#' character, and returns
+        the resulting list as a tuple.
+        """
+        # Realised that backtick code is unnecessary, but could be useful later
+        # if moderators want to mute people for X duration.
+        # Get leftmost backtick, and rightmost backtick
+        l_index = message.content.find('`')
+        r_index = message.content.rfind('`')
+        if (l_index == -1) or (r_index == -1) or (l_index == r_index):
+            await ctx.channel.send('Format looks incorrect. Please wrap the '
+                                   'username and four digits in backticks '
+                                   '(`).')
+            await ctx.channel.send('Cancelling.')
+            self.bot.processes['m_mute_member'] = None
+            return (None, None)
         
+        # Get username and the discriminator
+        username_four_digits = message.content[l_index+1:r_index]
+        return username_four_digits.split('#')
+    
+    async def get_user(self, ctx, username, digits):
+        """Returns a user object, given a username and their discriminator"""
+        # Get guild
+        with open('./server_specific/channel_ids.json', 'r') as id_file:
+            channel_id_dict = json.loads(id_file.read())
+        guild_id = channel_id_dict['GUILD']
+        guild = self.bot.get_guild(guild_id)
+
+        # Get user
+        user = discord.utils.get(guild.members, name=username, 
+                                 discriminator=digits)
+
+        # Check if the user does not exist
+        if user is None:
+            await ctx.channel.send('Could not find that user. Ensure that you '
+                                   'have entered their username and '
+                                   'discriminator correctly.')
+            await ctx.channel.send('Cancelling.')
+
+        return user
+
+    async def change_member_muted(self, ctx, user, isAdding):
+        # Get guild
+        with open('./server_specific/channel_ids.json', 'r') as id_file:
+            channel_id_dict = json.loads(id_file.read())
+        guild_id = channel_id_dict['GUILD']
+        guild = self.bot.get_guild(guild_id)
+
+        # Add or remove the role
+        if isAdding:
+            await user.add_roles(discord.utils.get(guild.roles, name='Muted'))
+            await ctx.channel.send('Muted user.')
+            self.bot.processes['m_mute_member'] = None
+        else:
+            await user.remove_roles(discord.utils.get(guild.roles, 
+                                                      name='Muted'))
+            await ctx.channel.send('Unmuted user.')
+            self.bot.processes['m_unmute_member'] = None
+
+    async def get_username_four_digits_msg(self, ctx):
         # The check for messages in Seng conversation
         def check(m):
             return (
@@ -36,6 +89,22 @@ class MuteMember(commands.Cog):
                        'user to mute, wrapped in backticks (\`). For example, '
                        '`toxicPerson#1773`. Write \'cancel\' to cancel.')
         member_req_msg = await self.bot.wait_for('message', check=check)
+        return member_req_msg
+
+
+    @commands.command()
+    async def m_mute_member(self, ctx):
+        """Mutes a Member"""
+        # Validation
+        if not await is_moderator(ctx):
+            return
+        if not await is_mod_commands_channel(ctx):
+            return
+        if not await is_process_and_user_clear(self.bot, 'm_mute_member', ctx):
+            return
+        
+        # Get the message containing the member moderator wants to mute
+        member_req_msg = await self.get_username_four_digits_msg(ctx)
 
         # Check if user wants to cancel
         if member_req_msg.content == 'cancel':
@@ -43,46 +112,20 @@ class MuteMember(commands.Cog):
             self.bot.processes['m_mute_member'] = None
             return
         
-        # Realised that backtick code is unnecessary, but could be useful later
-        # if moderators want to mute people for X duration.
-        # Get leftmost backtick, and rightmost backtick
-        l_index = member_req_msg.content.find('`')
-        r_index = member_req_msg.content.rfind('`')
-        if (l_index == -1) or (r_index == -1) or (l_index == r_index):
-            await ctx.channel.send('Format looks incorrect. Please wrap the '
-                                   'username and four digits in backticks '
-                                   '(`).')
-            await ctx.channel.send('Cancelling.')
-            self.bot.processes['m_mute_member'] = None
+        # Get the username and four digits
+        uname, digits = await self.get_uname_digits(ctx, member_req_msg)
+        if uname is None:  # Cancel the method in case user was not found
             return
-        
-        # Get username and the discriminator
-        username_four_digits = member_req_msg.content[l_index+1:r_index]
-        username, four_digits = username_four_digits.split('#')
 
-        # Get guild
-        with open('./server_specific/channel_ids.json', 'r') as id_file:
-            channel_id_dict = json.loads(id_file.read())
-        guild_id = channel_id_dict['GUILD']
-        guild = self.bot.get_guild(guild_id)
-
-        # Get user
-        user = discord.utils.get(guild.members, name=username, 
-                                 discriminator=four_digits)
-
+        # Gets the user
+        user = await self.get_user(ctx, uname, digits)
         # If the user does not seem to exist
         if user is None:
-            await ctx.channel.send('Could not find that user. Ensure that you '
-                                   'have entered their username and '
-                                   'discriminator correctly.')
-            await ctx.channel.send('Cancelling.')
             self.bot.processes['m_mute_member'] = None
             return
         
         # Assign mute to that user
-        await user.add_roles(discord.utils.get(guild.roles, name='Muted'))
-        await ctx.channel.send('Muted user.')
-        self.bot.processes['m_mute_member'] = None
+        await self.change_member_muted(ctx, user, True)
 
     @commands.command()
     async def m_unmute_member(self, ctx):
@@ -95,21 +138,9 @@ class MuteMember(commands.Cog):
         if not await is_process_and_user_clear(self.bot, 'm_unmute_member',
                                                ctx):
             return
-        
-        # The check for messages in Seng conversation
-        def check(m):
-            return (
-                asyncless_is_mod_commands_channel(m) and
-                m.author.id == ctx.author.id and
-                not m.content.startswith('$m')
-            )
 
-        # Ask for username and four digit discriminator
-        await ctx.send('Please enter the username and the four digits of the '
-                       'user to unmute, wrapped in backticks (\`). For '
-                       'example, `toxicPerson#1773`. Write \'cancel\' to '
-                       'cancel.')
-        member_req_msg = await self.bot.wait_for('message', check=check)
+        # Get the message containing the member moderator wants to mute
+        member_req_msg = await self.get_username_four_digits_msg(ctx)
 
         # Check if user wants to cancel
         if member_req_msg.content == 'cancel':
@@ -117,46 +148,20 @@ class MuteMember(commands.Cog):
             self.bot.processes['m_unmute_member'] = None
             return
         
-        # Realised that backtick code is unnecessary, but could be useful later
-        # if moderators want to mute people for X duration.
-        # Get leftmost backtick, and rightmost backtick
-        l_index = member_req_msg.content.find('`')
-        r_index = member_req_msg.content.rfind('`')
-        if (l_index == -1) or (r_index == -1) or (l_index == r_index):
-            await ctx.channel.send('Format looks incorrect. Please wrap the '
-                                   'username and four digits in backticks '
-                                   '(`).')
-            await ctx.channel.send('Cancelling.')
-            self.bot.processes['m_unmute_member'] = None
+        # Get the username and four digits
+        uname, digits = await self.get_uname_digits(ctx, member_req_msg)
+        if uname is None:  # Cancel the method in case user was not found
             return
-        
-        # Get username and the discriminator
-        username_four_digits = member_req_msg.content[l_index+1:r_index]
-        username, four_digits = username_four_digits.split('#')
 
-        # Get guild
-        with open('./server_specific/channel_ids.json', 'r') as id_file:
-            channel_id_dict = json.loads(id_file.read())
-        guild_id = channel_id_dict['GUILD']
-        guild = self.bot.get_guild(guild_id)
-
-        # Get user
-        user = discord.utils.get(guild.members, name=username, 
-                                 discriminator=four_digits)
-
+        # Gets the user
+        user = await self.get_user(ctx, uname, digits)
         # If the user does not seem to exist
         if user is None:
-            await ctx.channel.send('Could not find that user. Ensure that you '
-                                   'have entered their username and '
-                                   'discriminator correctly.')
-            await ctx.channel.send('Cancelling.')
             self.bot.processes['m_unmute_member'] = None
             return
         
         # Assign mute to that user
-        await user.remove_roles(discord.utils.get(guild.roles, name='Muted'))
-        await ctx.channel.send('Unmuted user.')
-        self.bot.processes['m_unmute_member'] = None
+        await self.change_member_muted(ctx, user, False)
 
     @commands.command()
     async def m_view_muted_members(self, ctx):
